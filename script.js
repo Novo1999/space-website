044c8fcb538b9c18aed3922af5406e9d7a6570b1
Novo <120156959+Novo1999@users.noreply.github.com>
2023-06-03 19:23:14 +0600

    ok

--- script.js
+++ script.js
@@ -6,18 +6,30 @@ const planetBg = document.querySelector('.planet-bg');
 const headline = document.querySelector('.headline');
 const exploreBtn = document.querySelector('.exploreBtn');
 const planet = document.querySelector('.planets');
-
+function homeActiveState() {
+  document.querySelector('.home').classList.add('tab-border');
+  document.querySelector('.moon').classList.add('mini-tab-border');
+}
+homeActiveState();
 navBtn.forEach(btn =>
   btn.addEventListener('click', e => {
+    btn.classList.remove('tab-border');
     if (e.currentTarget.classList.contains('home')) {
+      modifyStyle(navBtn, e);
       showTabContent('block', 'grid', 'none');
     }
     if (e.currentTarget.classList.contains('dest')) {
+      modifyStyle(navBtn, e);
       showTabContent('none', 'none', 'block');
       destination.style.display = 'grid';
     }
   })
 );
+
+function modifyStyle(navBtn, e) {
+  navBtn.forEach(btn => btn.classList.remove('tab-border'));
+  e.currentTarget.classList.add('tab-border');
+}
 
 // disp1 = block
 // disp2 = grid
@@ -33,4 +45,6 @@ function showTabContent(disp1, disp2, disp3) {
 exploreBtn.addEventListener('click', () => {
   showTabContent('none', 'none', 'block');
   destination.style.display = 'grid';
+  document.querySelector('.dest').classList.add('tab-border');
+  document.querySelector('.home').classList.remove('tab-border');
 });
